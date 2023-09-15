e7da7d19fe0f3f876dfbc81fcad2da3c396d85bb
Denis Lozko <dlozko@mail.ru>
2023-09-15 21:39:58 +0300

    Дополнения 20

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -84,23 +84,23 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 #DATABASES = {
 #    'default': {
-#        'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.postgresql'),
-#        'NAME': os.getenv('DB_NAME', 'postgres'),
-#        'USER': os.getenv('POSTGRES_USER', 'postgres'),
-#        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'postgres'),
-#        'HOST': os.getenv('DB_HOST', '127.0.0.1'),
-#        'PORT': os.getenv('DB_PORT', '5432')
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
 #    }
 #}
+
+DATABASES = {
+    'default': {
+        'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.postgresql'),
+        'NAME': os.getenv('POSTGRES_DB', 'postgres'),
+        'USER': os.getenv('POSTGRES_USER', 'postgres'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'postgres'),
+        'HOST': os.getenv('DB_HOST', '127.0.0.1'),
+        'PORT': os.getenv('DB_PORT', '5432')
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
