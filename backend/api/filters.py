5be9f630c6c723fc6ddc998d59636e5659cfb737
Denis Lozko <dlozko@mail.ru>
2023-10-08 20:47:44 +0300

    исправление 31

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -1,15 +1,18 @@
-from django_filters.rest_framework import FilterSet, filters
+from django_filters.rest_framework.filters import (BooleanFilter,
+                                                   ModelMultipleChoiceFilter,
+                                                   CharFilter)
+from django_filters.rest_framework import FilterSet
 from recipes.models import Ingredient, Recipe, Tag
 
 
 class RecipeFilter(FilterSet):
     """Фильтр выборки рецептов по определенным полям."""
-    is_favorited = filters.BooleanFilter(method='get_is_favorited')
-    is_in_shopping_cart = filters.BooleanFilter(
+    is_favorited = BooleanFilter(method='get_is_favorited')
+    is_in_shopping_cart = BooleanFilter(
         method='get_is_in_shopping_cart')
-    tags = filters.ModelMultipleChoiceFilter(queryset=Tag.objects.all(),
-                                             field_name='tags__slug',
-                                             to_field_name='slug', )
+    tags = ModelMultipleChoiceFilter(queryset=Tag.objects.all(),
+                                     field_name='tags__slug',
+                                     to_field_name='slug', )
 
     class Meta:
         model = Recipe
@@ -27,7 +30,7 @@ class RecipeFilter(FilterSet):
 
 
 class IngredientFilter(FilterSet):
-    name = filters.CharFilter(lookup_expr='istartswith')
+    name = CharFilter(lookup_expr='istartswith')
 
     class Meta:
         model = Ingredient
