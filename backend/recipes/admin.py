81072370d481fb98b540b8318525b6941dc4ab61
Denis Lozko <dlozko@mail.ru>
2023-10-12 10:41:59 +0300

    исправление43

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
 
-from .models import (Favorite, Ingredient,
-                     Recipe, RecipeIngredient,
-                     ShoppingList, Tag)
+from .models import (Favorite, Ingredient, Recipe,
+                     RecipeIngredient, ShoppingList, Tag)
 
 
 class TagAdmin(admin.ModelAdmin):
