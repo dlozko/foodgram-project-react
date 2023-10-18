3b3f71cbd7aa2c541995dc5a87f7bb6b12ee7415
Denis Lozko <dlozko@mail.ru>
2023-10-18 19:24:23 +0300

    исправление замечаний 10

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -119,7 +119,7 @@ AUTH_USER_MODEL = 'users.User'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticateds',
+        'rest_framework.permissions.IsAuthenticated',
     ],
 
     'DEFAULT_AUTHENTICATION_CLASSES': [
