df9e604c8acd382b89002530475652ffdc8c223d
Denis Lozko <118303678+dlozko@users.noreply.github.com>
2023-10-18 08:48:14 +0300

    Update2 settings.py

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -125,7 +125,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
     ],
-    'DEFAULT_PAGINATION_CLASS': 'api.paginators.CustomPagination',
+    'DEFAULT_PAGINATION_CLASS': 'api.pagination.CustomPagination',
     'PAGE_SIZE': 6,
 }
 
